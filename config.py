e796372c295bb32f959a990b493a26bdb4d80627
shivamkr0037 <130444141+shivamkr0037@users.noreply.github.com>
2023-04-14 12:10:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-TOKEN = '...' #Сюда вы пишите свой токен. Узнать его можно в @BotFather  
+TOKEN = '6117295473:AAHwPhdLMGtPI2ylGzdti2mBYBXASuDQvus' #Сюда вы пишите свой токен. Узнать его можно в @BotFather  
